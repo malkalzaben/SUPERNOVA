7ba90b4e94183284d6d1d67dff76bda93193821f
malkalzaben <120247397+malkalzaben@users.noreply.github.com>
2026-02-19 21:05:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-sed -i 's/\r$//' main.py install.sh
+
 if [ "$EUID" -ne 0 ]; then
-  echo "[!] Please run as root (use sudo ./install.sh)"
+  echo "[!] Please run as root (use sudo bash install.sh)"
   exit
 fi
+
+echo "[*] Cleaning Windows CRLF formats..."
+sed -i 's/\r$//' main.py
 
 echo "[*] Installing SUPERNOVA Scanner..."
 
 chmod +x main.py
 
 TOOL_DIR=$(pwd)
-
 ln -sf "$TOOL_DIR/main.py" /usr/local/bin/supernova
 
 echo "[+] Installation complete!"
