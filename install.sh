4f40668bd593fa32b165ad7a2c0cb97e5f915a31
malkalzaben <120247397+malkalzaben@users.noreply.github.com>
2026-02-19 15:16:06 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "[!] Please run as root (use sudo ./install.sh)"
+  exit
+fi
+
+echo "[*] Installing SUPERNOVA Scanner..."
+
+chmod +x main.py
+
+TOOL_DIR=$(pwd)
+
+ln -sf "$TOOL_DIR/main.py" /usr/local/bin/supernova
+
+echo "[+] Installation complete!"
+echo "[+] You can now run the tool from anywhere by typing: supernova"
