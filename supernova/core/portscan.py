af5437fd9919533c10d9d581da61b408948e34c7
malkalzaben <120247397+malkalzaben@users.noreply.github.com>
2026-02-19 17:37:59 +0300

    Update portscan.py

--- supernova/core/portscan.py
+++ supernova/core/portscan.py
@@ -32,25 +32,3 @@ def scan_multiple_ports(ip_address, ports_list, timeout_sec=1.0, max_threads=50)
                 open_ports.append(port_result)
 
     return sorted(open_ports)
-
-
-
-# testing
-# ==========================================
-if __name__ == "__main__":
-    target_ip = "192.168.100.1"
-
-    ports_to_scan = [21, 22, 23, 25, 53, 80, 110, 135, 139, 143, 443, 445, 3306, 3389, 8080]
-
-    scan_timeout = 0.5
-    print(f"[*] Starting Port Scan on {target_ip}...")
-    print(f"[*] Scanning {len(ports_to_scan)} common ports...\n")
-
-    found_ports = scan_multiple_ports(target_ip, ports_to_scan, timeout_sec=scan_timeout)
-
-    if found_ports:
-        print(f"[+] Scan Completed! Open Ports found:")
-        for p in found_ports:
-            print(f"    - Port {p} is OPEN")
-    else:
-        print("[-] Scan Completed. No open ports found from the list.")
